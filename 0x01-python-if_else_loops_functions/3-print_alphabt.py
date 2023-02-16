0dd03ce7a1352e74793f20a3e1dd548b059e55e3
Ogun6ele <kehindegundel@gmail.com>
2023-02-16 05:22:42 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for xup in range(ord('a'), ord('b') + 1):
+for xup in range(ord('a'), ord('z')):
     if chr(xup) != 'q' and chr(xup) != 'e':
         print("{}".format(chr(xup)), end='')
