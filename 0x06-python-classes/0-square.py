204905e77fa09913c9394bc92b4dc923dfff874b
Ogun6ele <kehindegundel@gmail.com>
2023-02-25 14:15:30 -0800

    first commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author: KEHINDE OGUNDELE
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Author: KEHINDE OGUNDELE
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
