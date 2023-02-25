3de054117779c3fb875d08783ad61aa1edbb4aa2
Ogun6ele <kehindegundel@gmail.com>
2023-02-25 07:36:35 -0800

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     length = len(sys.argv) - 1
     if length == 0:
         print("{} arguments.".format(length))
-    elif length  == 1:
+    elif length == 1:
         print("{} argument:".format(length))
     else:
         print("{} arguments:".format(length))
