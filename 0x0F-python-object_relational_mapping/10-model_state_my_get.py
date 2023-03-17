e75b54d3281ef9679e8aa07c9c084198772d56c8
Ogun6ele <kehindegundel@gmail.com>
2023-03-16 23:54:04 -0700

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -3,25 +3,22 @@
 with the given name
 """
 import sys
-from model_state import Base, State
-from sqlalchemy import create_engine, asc
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-
-def main(argv):
-    """ main function """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    try:
-        instance = session.query(State).filter(State.name == argv[4]).one()
-        print(str(instance.id))
-    except:
+
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
         print("Not found")
-    session.close()
-
-
-if __name__ == '__main__' and len(sys.argv) == 5:
-    main(sys.argv)
